ba2c123d9c2dc0b9b23e70a67e9a1a66a77d5faa
Petr Čech <Petr ech@LAPTOP-5T240PPN>
2022-11-08 10:32:08 +0100

    A lot

---
+++ TygrISServer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TygrISServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
